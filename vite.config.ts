617f1bb9566607049b4730f33f009d8c5d9104ce
Rotha SAMON <samonrotha@gmail.com>
2024-11-22 15:11:00 +0700

    added the github workflow for Ci/CD

--- vite.config.ts
+++ vite.config.ts
@@ -6,18 +6,32 @@
 //   plugins: [react()],
 // })
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/", // Base URL for your app
+//   build: {
+//     outDir: "dist", // Output directory
+//     assetsDir: "assets", // Directory for static assets
+//   },
+//   server: {
+//     port: 3000, // Local development port (optional)
+//   },
+// });
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Base URL for your app
+  base: '/',  // Ensure that the base path is set to the root of your server
   build: {
-    outDir: "dist", // Output directory
-    assetsDir: "assets", // Directory for static assets
+    outDir: 'dist', // Output directory for the build
+    assetsDir: 'assets', // Directory for static assets
   },
-  server: {
-    port: 3000, // Local development port (optional)
-  },
-});
+})
+
